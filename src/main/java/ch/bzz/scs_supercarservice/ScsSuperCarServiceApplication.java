a3db2b8aa6f9dc29e26934aa4c0c13b467946530
Noah-Bargisen <bargisenn@bzz.ch>
2023-05-26 14:03:38 +0200

    springboot init

---
+++ src/main/java/ch/bzz/scs_supercarservice/ScsSuperCarServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.bzz.scs_supercarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsSuperCarServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScsSuperCarServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bzz/scs_supercarservice/ScsSuperCarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bzz.scs_supercarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsSuperCarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
