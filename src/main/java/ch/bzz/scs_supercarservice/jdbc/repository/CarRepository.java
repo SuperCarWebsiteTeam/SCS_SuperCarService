4f5d926b87bf8de71eaf28c2f3ae0d6479c4cccd
Noah Bargisen <bargisenn@bzz.ch>
2023-06-30 09:38:44 +0200

    [fix] openAPI type change and DataAccessService v1 done

--- src/main/java/ch/bzz/scs_supercarservice/jdbc/repository/CarRepository.java
+++ src/main/java/ch/bzz/scs_supercarservice/jdbc/repository/CarRepository.java
@@ -1,7 +1,23 @@
 package ch.bzz.scs_supercarservice.jdbc.repository;
 
 import ch.bzz.scs_supercarservice.jdbc.records.Car;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CarRepository extends JpaRepository<Car, Integer> {
+import java.util.List;
+
+public interface CarRepository extends JpaRepository<Car, Integer>, JpaSpecificationExecutor<Car> {
+    @Query("""
+            select c from Car c
+            where c.cityKpl = :cityKpl and c.classField = :classField and c.combinationKpl = :combinationKpl and c.cylinders = :cylinders and c.displacement = :displacement and c.drive = :drive and c.fuelType = :fuelType and c.highwayKpl = :highwayKpl and c.make = :make and c.model = :model and c.transmission = :transmission and c.year = :year""")
+    List<Car> findByCityKplAndClassFieldAndCombinationKplAndCylindersAndDisplacementAndDriveAndFuelTypeAndHighwayKplAndMakeAndModelAndTransmissionAndYear(@Param("cityKpl") Integer cityKpl, @Param("classField") String classField, @Param("combinationKpl") Integer combinationKpl, @Param("cylinders") Integer cylinders, @Param("displacement") Float displacement, @Param("drive") String drive, @Param("fuelType") String fuelType, @Param("highwayKpl") Integer highwayKpl, @Param("make") String make, @Param("model") String model, @Param("transmission") String transmission, @Param("year") Integer year, Pageable pageable);
+
+
+
+
+
+
 }
--- src/main/java/ch/bzz/scs_supercarservice/services/DataAccessService.java
+++ src/main/java/ch/bzz/scs_supercarservice/services/DataAccessService.java
@@ -1,7 +1,42 @@
 package ch.bzz.scs_supercarservice.services;
 
+import ch.bzz.scs_supercarservice.jdbc.repository.CarRepository;
+import com.scs.gen.v1.module.Car;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DataAccessService {
+
+    private CarRepository carRepository;
+
+    public DataAccessService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public List<Car> getCars(Integer cityKpl, String classField, Integer combinationKpl, Integer cylinders, Float displacement, String drive, String fuelType, Integer highwayKpl, String make, String model, String transmission, Integer year, Integer limit) {
+        List<ch.bzz.scs_supercarservice.jdbc.records.Car> cars = carRepository.findByCityKplAndClassFieldAndCombinationKplAndCylindersAndDisplacementAndDriveAndFuelTypeAndHighwayKplAndMakeAndModelAndTransmissionAndYear(cityKpl, classField, combinationKpl, cylinders, displacement, drive, fuelType, highwayKpl, make, model, transmission, year, Pageable.ofSize(limit));
+        List<Car> responseCars = new ArrayList<>();
+        for (ch.bzz.scs_supercarservice.jdbc.records.Car car: cars) {
+            Car responseCar = new Car();
+            responseCar.setCityKpl(car.getCityKpl());
+            responseCar.setPropertyClass(car.getClassField());
+            responseCar.setCombinationKpl(car.getCombinationKpl());
+            responseCar.setCylinders(car.getCylinders());
+            responseCar.setDisplacement(car.getDisplacement());
+            responseCar.setDrive(car.getDrive());
+            responseCar.setFuelType(car.getFuelType());
+            responseCar.setHighwayKpl(car.getHighwayKpl());
+            responseCar.setMake(car.getMake());
+            responseCar.setModel(car.getModel());
+            responseCar.setTransmission(car.getTransmission());
+            responseCar.setYear(car.getYear());
+            responseCars.add(responseCar);
+        }
+        return responseCars;
+    }
+
 }
